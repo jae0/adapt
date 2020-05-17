5be5a7aac23b70f65ffe10791aabc80fa4a4829f
Jae S Choi <choi.jae.seok@gmail.com>
2020-05-17 12:56:56 -0300

    clean up

---
+++ R/simulate_add_noise.R
@@ -0,0 +1,21 @@
+
+simulate_add_noise = function( sim, nobs=30 ) {
+
+  subsample = sort( sample.int( length(1:50), Nobs, replace=FALSE ) )
+
+  # missing data
+  missing = which(!is.finite(sim$S[subsample] ))
+  trange = range(sim$time[subsample])
+  out = data.frame(time=seq(trange[1], trange[2], by=1), S=NA, I=NA, R=NA )
+  with_data = match( sim$time[subsample], out$time)
+  out[with_data, "S" ] = as.integer(sim$S[subsample])
+  out[with_data, "I" ] = as.integer(sim$I[subsample])
+  out[with_data, "R" ] = as.integer(sim$R[subsample])
+
+  out$S[ which(!is.finite(out$S)) ] = -1 # reset NAs to Inf as stan does not take NAs
+  out$I[ which(!is.finite(out$I)) ] = -1 # reset NAs to Inf as stan does not take NAs
+  out$R[ which(!is.finite(out$R)) ] = -1 # reset NAs to Inf as stan does not take NAs
+
+  return(out)
+
+}
