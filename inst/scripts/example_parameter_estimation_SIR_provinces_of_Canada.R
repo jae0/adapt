53cebdfd97cbd837077f18ce4e465e7d5b7c506c
Jae S Choi <choi.jae.seok@gmail.com>
2020-06-09 21:51:50 -0300

    updates

--- inst/scripts/example_parameter_estimation_SIR_provinces_of_Canada.R
+++ inst/scripts/example_parameter_estimation_SIR_provinces_of_Canada.R
@@ -26,12 +26,12 @@ if ("download.data" %in% tasks) res = data_provinces_of_canada( selection="downl
 
 can = data_provinces_of_canada(
   fn = fn,
-  Npreds = 14,   # number of days for ode-based forward projections
+  Npreds = 7,   # number of days for ode-based forward projections
   BNP = 1,       # beta dynamics is AR(BNP) ; also the number of days to average for forward ode-based projections (incubation time is ~ 5-7 days) .. higher than 1 can cause problems ... high var in reporting causes + and - corrs
-  BETA_max = 1.0,     # max rate param for S -> I  # approx number of contacts per person per time (day) multiplied by the probability of disease transmission in a contact between a susceptible and an infectious subject;  ~ 1/( typical time in days between contacts)
+  BETA_max = 1.2,     # max rate param for S -> I  # approx number of contacts per person per time (day) multiplied by the probability of disease transmission in a contact between a susceptible and an infectious subject;  ~ 1/( typical time in days between contacts)
   # BETA_max is very important: seldom does this value go > 1 for Covid-19 in Canada,
-  GAMMA_max = 0.1,    # max rate param for I -> R  # ~ 1/(typical time until removal = 14) = 0.07
-  EPSILON_max = 0.05,  # max rate param for I -> M  # > recovery time; < rate ..
+  GAMMA_max = 0.2,    # max rate param for I -> R  # ~ 1/(typical time until removal = 14) = 0.07
+  EPSILON_max = 0.1,  # max rate param for I -> M  # > recovery time; < rate ..
   modelname="default"
 )
 
@@ -54,7 +54,7 @@ for (au in  provinces) {
     stan_results = list( stan_inputs=can[[au]] )
     fn_model = file.path( workdir, paste( au, stan_results$stan_inputs$modelname, "rdata", sep=".") )
     outdir = file.path( "~", "bio", "adapt", "inst", "doc", au)
-    control.stan = list(adapt_delta = 0.95, max_treedepth=16 )
+    control.stan = list(adapt_delta = 0.9, max_treedepth=15 )
 
     if ("model" %in% tasks ) {
       stan_results$stan_samples = rstan::sampling( stancode_compiled, data=stan_results$stan_inputs, chains=3, warmup=5000, iter=7500, control=control.stan  )
