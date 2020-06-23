2c32d937a4fee2d861f5f7fe88252c9f96f3b64b
Jae S Choi <choi.jae.seok@gmail.com>
2020-06-23 16:46:03 -0300

    updates

--- inst/scripts/example_parameter_estimation_SIR_provinces_of_Canada.R
+++ inst/scripts/example_parameter_estimation_SIR_provinces_of_Canada.R
@@ -56,10 +56,10 @@ for (au in  provinces) {
     stan_results = list( stan_inputs=can[[au]] )
     fn_model = file.path( workdir, paste( au, stan_results$stan_inputs$modelname, "rdata", sep=".") )
     outdir = file.path( "~", "bio", "adapt", "inst", "doc", au)
-    control.stan = list(adapt_delta = 0.9, max_treedepth=15 )
+    control.stan = list(adapt_delta = 0.925, max_treedepth=14 )
 
     if ("model" %in% tasks ) {
-      stan_results$stan_samples = rstan::sampling( stancode_compiled, data=stan_results$stan_inputs, chains=3, warmup=5000, iter=7500, control=control.stan  )
+      stan_results$stan_samples = rstan::sampling( stancode_compiled, data=stan_results$stan_inputs, chains=3, warmup=7000, iter=8000, control=control.stan  )
       save(stan_results, file=fn_model, compress=TRUE)
     } else {
       load(fn_model)
